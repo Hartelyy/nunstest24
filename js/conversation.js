3502457fb9d1c3463d477d6a1234d00449744a78
gyeonghun <erguono@gmail.com>
2024-09-03 21:04:02 +0900

    GA 이벤트 추가

--- js/conversation.js
+++ js/conversation.js
@@ -1,4 +1,4 @@
-import { scripts, scripts2, scripts3, scripts4, scripts5, scripts6, scripts8, 
+import { scripts, scripts2, scripts3, scripts4, scripts5, scripts6, scripts8,
   scripts9, scripts10, scripts11, scripts12, scripts13, scripts14, scripts15, scripts16} from './con_data.js'
 
 // 질문 화면의 각 요소를 찾아요!
@@ -13,7 +13,7 @@ const button = document.getElementById('darken');
 let currentNumber = 0; // 현재 질문 번호
 let currentScriptSetIndex = 0; // 현재 스크립트 세트 인덱스
 
-const scriptSets = [scripts, scripts2, scripts3, scripts4, scripts5, scripts6, scripts8, 
+const scriptSets = [scripts, scripts2, scripts3, scripts4, scripts5, scripts6, scripts8,
   scripts9, scripts10, scripts11, scripts12, scripts13, scripts14, scripts15, scripts16]; // 모든 스크립트 세트를 배열로
 
 // 화면에 질문을 랜더링하는 함수에요!
@@ -23,6 +23,12 @@ function renderQuestion() {
   scriptEl.innerHTML = script.script
   numberEl.innerHTML = script.number
   imgEl.style.backgroundImage = `url(${script.img})`; //img 경로 설정
+
+  gtag('event', 'transition', {
+    'event_category': 'Button',
+    'event_label': 'Buy Now Button',
+    'value': `${currentScriptSetIndex} / ${currentNumber}`
+  });
 
   if (script.choices) {
     choice1El.style.display = 'block';
@@ -38,9 +44,9 @@ function renderQuestion() {
 // 다음 질문으로 넘어가는 함수
 function nextQuestion() {
   const currentScriptSet = scriptSets[currentScriptSetIndex];
-  
+
   if (currentNumber === currentScriptSet.length - 1) {
-    
+
     // 스크립트 세트가 끝나면 다음 스크립트 세트로 넘어감
     if (currentScriptSetIndex < scriptSets.length - 1) {
       screenblack(true);
